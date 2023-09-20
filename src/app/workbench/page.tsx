6db61d13b2a38f2e83f140360373e6bef119597e
Anand-1432 <123agaikwad@gmail.com>
2023-09-20 19:56:23 +0530

    anand chnages

---
+++ src/app/workbench/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./style.scss"
+import SideBar from '@/components/sidebar/SideBar'
+
+
+const page = () => {
+  return (
+    <>
+      <div className='workHome'>
+        <SideBar/>
+      </div>
+    </>
+  )
+}
+
+export default page
