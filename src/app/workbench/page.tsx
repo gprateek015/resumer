b40547a4d26e0064ec71bd61e6ffa37690f72d45
Anand-1432 <123agaikwad@gmail.com>
2023-09-21 08:59:20 +0530

    auto fill

--- src/app/workbench/page.tsx
+++ src/app/workbench/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import "./style.scss"
 import SideBar from '@/components/sidebar/SideBar'
 import { Button, Grid } from '@mui/material'
@@ -15,15 +15,83 @@ import GridViewIcon from '@mui/icons-material/GridView';
 import DescriptionIcon from '@mui/icons-material/Description';
 import AirplayIcon from '@mui/icons-material/Airplay';
 import HelpIcon from '@mui/icons-material/Help';
+import { useSelector } from 'react-redux';
+import { useForm } from 'react-hook-form';
 
 
 const page = () => {
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue
+  } = useForm();
 
   const [first, setFirst] = useState(false);
   const [second, setSecond] = useState(false);
   const [third, setThird] = useState(false);
   const [fourth, setFourth] = useState(false);
+
+
+  const { resumeData } = useSelector((state: any) => state.user);
+
+  useEffect(() => {
+    if (resumeData) {
+      console.log(resumeData);
+      setValue("fname", String(resumeData.name).split(" ")[0]);
+      setValue("lname", String(resumeData.name).split(" ")[1]);
+      setValue("phone", resumeData.phone);
+      setValue("email", resumeData.email);
+      setValue("city", resumeData.city);
+      setValue("state", resumeData.state);
+      setValue("nationality", "Indian");
+      setValue("country", "India");
+
+      setValue("company_name", resumeData.experiences[0].company_name);
+      setValue("start_date", resumeData.experiences[0].start_date);
+      setValue("end_date", resumeData.experiences[0].end_date);
+      setValue("position", resumeData.experiences[0].position);
+      setValue("mode", resumeData.experiences[0].mode);
+      setValue("description", resumeData.experiences[0].description[0]);
+
+      setValue("institute_name", resumeData.educations[0].institute_name);
+      setValue("education_type", resumeData.educations[0].education_type);
+      setValue("specialisation", resumeData.educations[0].specialisation);
+      setValue("start_year", resumeData.educations[0].start_year);
+      setValue("score", resumeData.educations[0].score);
+
+      let techString = ""
+      for (let i = 0; i < resumeData.technical_skills.length; i++) {
+        techString += resumeData.technical_skills[i] + ", ";
+      }
+      setValue("technical_skills", techString);
+
+      let langString = ""
+      for (let i = 0; i < resumeData.languages.length; i++) {
+        langString += resumeData.languages[i] + ", ";
+      }
+      setValue("languages", langString);
+
+      let coreStr = "";
+      for (let i = 0; i < resumeData.core_subjects.length; i++) {
+        coreStr += resumeData.core_subjects[i] + ", ";
+      }
+      setValue("core_subjects", coreStr);
+
+      let devStr = "";
+      for (let i = 0; i < resumeData.dev_tools.length; i++) {
+        devStr += resumeData.dev_tools[i] + ", ";
+      }
+      setValue("dev_tools", devStr);
+
+    }
+  }, [resumeData]);
+
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  }
 
 
   return (
@@ -40,6 +108,7 @@ const page = () => {
           <div className='innerGrid'>
 
             <div className='leftGrid'>
+              <form onSubmit={handleSubmit(onSubmit)}></form>
 
 
               {/* //-------------------------------- first service ----------------------------------------// */}
@@ -56,22 +125,22 @@ const page = () => {
                       <div className='inpGrid'>
                         <div>
                           <label htmlFor="fname">First Name</label>
-                          <input type="text" name="" id="fname" />
+                          <input type="text" id="fname" {...register('fname')} />
                         </div>
                         <div>
                           <label htmlFor="lname">Last Name</label>
-                          <input type="text" name="" id="lname" />
+                          <input type="text" id="lname" {...register('lname')} />
                         </div>
                       </div>
 
                       <div className='inputTag'>
                         <label htmlFor="phone">Phone Number</label>
-                        <input type="text" id='phone' />
+                        <input type="text" id='phone' {...register('phone')} />
                       </div>
 
                       <div className='inputTag'>
                         <label htmlFor="email">Email</label>
-                        <input type="text" id='email' />
+                        <input type="text" id='email' {...register('email')} />
                       </div>
                     </div>
                   </div>
@@ -84,7 +153,7 @@ const page = () => {
                       </div>
                       <div>
                         <label htmlFor="lname">Nationality</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('nationality')} />
                       </div>
                     </div>
                     <div className='inpGrid'>
@@ -94,17 +163,17 @@ const page = () => {
                       </div>
                       <div>
                         <label htmlFor="lname">city</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('city')} />
                       </div>
                     </div>
                     <div className='inpGrid'>
                       <div>
-                        <label htmlFor="fname">Postal</label>
-                        <input type="text" name="" id="fname" />
+                        <label htmlFor="fname">State</label>
+                        <input type="text" id="fname" {...register('state')} />
                       </div>
                       <div>
                         <label htmlFor="lname">Country</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('country')} />
                       </div>
                     </div>
                     <div className='inputTag'>
@@ -130,38 +199,38 @@ const page = () => {
                     <div className='inpGrid'>
                       <div>
                         <label htmlFor="fname">Company name</label>
-                        <input type="text" name="" id="fname" />
+                        <input type="text" id="fname" {...register('company_name')} />
                       </div>
                       <div>
                         <label htmlFor="lname">Job title</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('position')} />
                       </div>
                     </div>
                     <div className='inpGrid'>
                       <div>
-                        <label htmlFor="fname">City</label>
-                        <input type="text" name="" id="fname" />
+                        <label htmlFor="fname">mode</label>
+                        <input type="text" id="fname" {...register('mode')} />
                       </div>
                       <div>
                         <label htmlFor="lname">Country</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('country')} />
                       </div>
                     </div>
 
                     <div className='inpGrid'>
                       <div>
                         <label htmlFor="fname">Start Date</label>
-                        <input type="text" name="" id="fname" />
+                        <input type="text" id="fname" {...register('start_date')} />
                       </div>
                       <div>
                         <label htmlFor="lname">End Date</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('end_date')} />
                       </div>
                     </div>
 
                     <div className='inputTag'>
                       <label htmlFor="email">Description</label>
-                      <input type="text" id='email' />
+                      <input type="text" id='email' {...register('description')} />
                     </div>
 
 
@@ -184,28 +253,28 @@ const page = () => {
                     <div className='inpGrid'>
                       <div>
                         <label htmlFor="fname">Institution name</label>
-                        <input type="text" name="" id="fname" />
+                        <input type="text" id="fname" {...register('institute_name')} />
                       </div>
                       <div>
                         <label htmlFor="lname">Field of study</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('specialisation')} />
                       </div>
                     </div>
                     <div className='inpGrid'>
                       <div>
                         <label htmlFor="fname">Degree</label>
-                        <input type="text" name="" id="fname" />
+                        <input type="text" id="fname" {...register('education_type')} />
                       </div>
                       <div></div>
                     </div>
                     <div className='inpGrid'>
                       <div>
-                        <label htmlFor="fname">City</label>
-                        <input type="text" name="" id="fname" />
+                        <label htmlFor="fname">Start Year</label>
+                        <input type="text" id="fname" {...register('start_year')} />
                       </div>
                       <div>
-                        <label htmlFor="lname">Country</label>
-                        <input type="text" name="" id="lname" />
+                        <label htmlFor="lname">Score</label>
+                        <input type="text" id="lname" {...register('score')} />
                       </div>
                     </div>
 
@@ -231,7 +300,7 @@ const page = () => {
                       </div>
                       <div>
                         <label htmlFor="lname">Skill level</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('technical_skills')} />
                       </div>
                     </div>
                     <div className='inpGrid'>
@@ -241,7 +310,7 @@ const page = () => {
                       </div>
                       <div>
                         <label htmlFor="lname">Skill level</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('core_subjects')} />
                       </div>
                     </div>
                     <div className='inpGrid'>
@@ -251,7 +320,7 @@ const page = () => {
                       </div>
                       <div>
                         <label htmlFor="lname">Skill level</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('languages')} />
                       </div>
                     </div>
                     <div className='inpGrid'>
@@ -261,7 +330,7 @@ const page = () => {
                       </div>
                       <div>
                         <label htmlFor="lname">Skill level</label>
-                        <input type="text" name="" id="lname" />
+                        <input type="text" id="lname" {...register('dev_tools')} />
                       </div>
                     </div>
 
