13015aa50a5934bc2224bd10916a6df9411797f9
Whycodex <neerajgupta12300@gmail.com>
2024-02-19 05:29:18 +0530

    Updated job description page

--- src/app/job-description/page.tsx
+++ src/app/job-description/page.tsx
@@ -11,22 +11,35 @@ const righteous = Righteous({
 const JobDescription = () => {
   return (
     <Grid
-      className={righteous.className}
-      sx={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: '20px',
+      padding: { xs: '20px', md: '20px 40px' },
+      width: '100%',
+      height: 'calc(100vh - 120px)',
+      maxHeight: { xs: '90%', md: '600px' }
+    }}
+    className={righteous.className}
     >
       <Typography
-        fontSize={'35px'}
         sx={{
+          background:
+            'linear-gradient(90deg, #4ADFD5 0.42%, #7479FA 41.67%, #E92EC3 106.58%)',
+          color: 'transparent',
+          backgroundClip: 'text',
+          WebkitBackgroundClip: 'text',
+          width: 'fit-content',
+          fontWeight: '400',
+          fontSize: '2rem',
+          lineHeight: '2.3rem',
+          letterSpacing: '1.1px',
           padding: '30px'
         }}
       >
-        Please paste in your job description here
+        Please provide us with the job description
       </Typography>
       <DescriptionForm />
     </Grid>
--- src/components/job-description/index.tsx
+++ src/components/job-description/index.tsx
@@ -1,31 +1,31 @@
-'use client';
+"use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import {
   Grid,
   TextField,
   Typography,
   Button,
   CircularProgress,
-  Backdrop
-} from '@mui/material';
-import icon from '@/assets/onboarding7.png';
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { useDispatch } from '@/redux/store';
-import { generateResumeData } from '@/actions/resume';
+  Backdrop,
+} from "@mui/material";
+import icon from "@/assets/onboarding7.png";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useDispatch } from "@/redux/store";
+import { generateResumeData } from "@/actions/resume";
 
 const DescriptionForm = () => {
   const dispatch = useDispatch();
   const route = useRouter();
-  const [jobDescription, setJobDescription] = useState('');
+  const [jobDescription, setJobDescription] = useState("");
   const [loading, setLoading] = useState(false);
 
   const getResumeData = async () => {
     setLoading(true);
     await dispatch(generateResumeData({ jobDescription }));
     setLoading(false);
-    route.push('/workbench');
+    route.push("/workbench");
   };
 
   return (
@@ -34,72 +34,80 @@ const DescriptionForm = () => {
         <Backdrop
           open={loading}
           sx={{
-            zIndex: '10'
+            zIndex: "10",
           }}
         >
-          <CircularProgress sx={{ color: 'white' }} />
+          <CircularProgress sx={{ color: "white" }} />
         </Backdrop>
       )}
       <Grid
         sx={{
-          width: { xs: '360px', md: '1100px' },
-          height: '450px',
-          borderRadius: '20px',
-          background: 'rgba(255, 255, 255, 0.10)',
-          backdropFilter: 'blur(20px)',
-          padding: '26px',
-          display: 'flex',
-          flexDirection: 'row',
-          overflow: 'auto',
-          '&::-webkit-scrollbar': {
-            display: 'none'
+          width: { xs: "360px", md: "900px" },
+          borderRadius: "20px",
+          background: "rgba(255, 255, 255, 0.10)",
+          backdropFilter: "blur(20px)",
+          padding: "26px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: {xs: "column", md:"row"},
+          overflow: "auto",
+          "&::-webkit-scrollbar": {
+            display: "none",
           },
-          MsOverflowStyle: 'none',
-          scrollbarWidth: 'none'
+          MsOverflowStyle: "none",
+          scrollbarWidth: "none",
         }}
       >
+        <Grid sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}>
+          <Image
+            src={icon}
+            alt="icon"
+            style={{
+              height: "250px",
+              width: "250px",
+            }}
+            priority={true}
+          />
+        </Grid>
         <Grid
           sx={{
-            width: { xs: '360px', md: '500px' },
-            height: '400px',
-            borderRadius: '20px',
-            background: 'rgba(255, 255, 255, 0.10)',
-            backdropFilter: 'blur(20px)',
-            padding: '12px',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center'
+            width: { xs: "360px", md: "100%" },
+            borderRadius: "20px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-        >
-          <Typography textAlign='center'>
-            Please enter job description below
-          </Typography>
+          >
+          <Typography>paste in your job description here</Typography>
           <TextField
             fullWidth
             multiline
-            rows={12}
+            rows={9}
             hidden
-            placeholder='Start typing in here'
+            placeholder="Start typing in here"
             inputProps={{
               sx: {
-                background: 'white',
-                padding: '7px 10px',
-                height: '300px',
-                border: '1px solid #E9E9E9',
-                borderRadius: '4px',
-                lineHeight: '20px',
-                marginBottom: '10px'
-              }
+                background: "white",
+                padding: "7px 10px",
+                border: "1px solid #E9E9E9",
+                borderRadius: "4px",
+                lineHeight: "20px",
+              },
             }}
-            onChange={e => setJobDescription(e.target.value)}
+            onChange={(e) => setJobDescription(e.target.value)}
           />
           <Button
             sx={{
-              borderRadius: '10px',
-              border: '1px solid #FFF',
-              background: '#FFF',
-              padding: '10px',
-              width: '90%'
+              borderRadius: "10px",
+              border: "1px solid #FFF",
+              background: "#FFF",
+              padding: "10px",
+              width: "95%",
             }}
             fullWidth
             onClick={() => getResumeData()}
@@ -107,120 +115,18 @@ const DescriptionForm = () => {
             <Typography
               sx={{
                 background:
-                  'linear-gradient(90deg, #4ADFD5 0.42%, #7479FA 41.67%, #E92EC3 106.58%)',
-                backgroundClip: 'text',
-                WebkitBackgroundClip: 'text',
-                WebkitTextFillColor: 'transparent',
-                fontSize: '14px',
-                fontWeight: '600',
-                width: '155px',
-                letterSpacing: '1px'
+                  "linear-gradient(90deg, #4ADFD5 0.42%, #7479FA 41.67%, #E92EC3 106.58%)",
+                backgroundClip: "text",
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                fontSize: "14px",
+                fontWeight: "600",
+                letterSpacing: "1px",
               }}
             >
               Generate Resume
             </Typography>
           </Button>
-        </Grid>
-        <Grid
-          sx={{
-            display: 'flex',
-            width: '100%'
-          }}
-        >
-          <Grid>
-            <Typography sx={{ marginTop: '10px' }} textAlign='center'>
-              ----- Or -----
-            </Typography>
-            <Image
-              src={icon}
-              alt='icon'
-              style={{
-                height: '350px',
-                width: '350px',
-                marginTop: '5%'
-              }}
-              priority={true}
-            />
-          </Grid>
-          <Grid
-            sx={{
-              width: { xs: '360px', md: '340px' },
-              height: '400px',
-              borderRadius: '20px',
-              background: 'rgba(255, 255, 255, 0.10)',
-              backdropFilter: 'blur(20px)',
-              padding: '20px',
-              marginLeft: '5%'
-            }}
-          >
-            <Typography
-              style={{
-                marginBottom: 40
-              }}
-              fontSize={16}
-              textAlign='center'
-            >
-              Do you have a job ID then please provide the details below
-            </Typography>
-            <Typography paddingBottom={1}>Enter company name</Typography>
-            <TextField
-              fullWidth
-              placeholder='Google'
-              inputProps={{
-                sx: {
-                  background: 'white',
-                  padding: '7px 10px',
-                  height: '30px',
-                  border: '1px solid #E9E9E9',
-                  borderRadius: '4px',
-                  lineHeight: '20px',
-                  marginBottom: '30px'
-                }
-              }}
-            />
-            <Typography paddingBottom={1}>Enter job ID</Typography>
-            <TextField
-              fullWidth
-              placeholder='hireme'
-              inputProps={{
-                sx: {
-                  background: 'white',
-                  padding: '7px 10px',
-                  height: '30px',
-                  border: '1px solid #E9E9E9',
-                  borderRadius: '4px',
-                  lineHeight: '20px'
-                }
-              }}
-            />
-            <Button
-              sx={{
-                borderRadius: '10px',
-                border: '1px solid #FFF',
-                background: '#FFF',
-                padding: '10px',
-                marginTop: '40px'
-              }}
-              fullWidth
-              onClick={() => getResumeData()}
-            >
-              <Typography
-                sx={{
-                  background:
-                    'linear-gradient(90deg, #4ADFD5 0.42%, #7479FA 41.67%, #E92EC3 106.58%)',
-                  backgroundClip: 'text',
-                  WebkitBackgroundClip: 'text',
-                  WebkitTextFillColor: 'transparent',
-                  fontSize: '14px',
-                  fontWeight: '600',
-                  width: '155px',
-                  letterSpacing: '1px'
-                }}
-              >
-                Generate Resume
-              </Typography>
-            </Button>
-          </Grid>
         </Grid>
       </Grid>
     </>
