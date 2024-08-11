31b681f18a97ff80cb0451ac54c8fe5449f2992e
Prateek Goyal <gprateek015@gmail.com>
2024-08-12 00:29:08 +0530

    chore: fix build issues

--- src/app/(protected-routes)/workbench/components/skills.tsx
+++ src/app/(protected-routes)/workbench/components/skills.tsx
@@ -1,25 +1,25 @@
-import { Chip, Grid, Typography } from '@mui/material';
-import React, { useCallback, useEffect, useState } from 'react';
-import SchoolIcon from '@mui/icons-material/School';
-import WorkExpDetailDesign from '@/components/work-experiences/detail';
-import { SubmitHandler, useFieldArray, useFormContext } from 'react-hook-form';
-import { Experience, Skill } from '@/types';
-import EduDetailDesign from '@/components/educations/detail';
-import ProjectDetailDesign from '@/components/projects/detail';
-import SkillsInput, { CreatableSkill } from '@/components/skills-input';
+import { Chip, Grid, Typography } from "@mui/material";
+import React, { useCallback, useEffect, useState } from "react";
+import SchoolIcon from "@mui/icons-material/School";
+import WorkExpDetailDesign from "@/components/work-experiences/detail";
+import { SubmitHandler, useFieldArray, useFormContext } from "react-hook-form";
+import { Experience, Skill } from "@/types";
+import EduDetailDesign from "@/components/educations/detail";
+import ProjectDetailDesign from "@/components/projects/detail";
+import SkillsInput, { CreatableSkill } from "@/components/skills-input";
 
-import { useDrop } from 'react-dnd';
-import SkillsContainer from '@/components/profile-details/skills';
-import Heading from './heading';
-import { DragSkillItem } from '../../../components/profile-details/dragable-chip';
+import { useDrop } from "react-dnd";
+import SkillsContainer from "@/components/profile-details/skills";
+import Heading from "./heading";
+import { DragSkillItem } from "@/components/profile-details/dragable-chip";
 
 type OnChangeValueType =
   | CreatableSkill
-  | { name: string; type: Skill['type'] | 'new_skill'; value?: string };
+  | { name: string; type: Skill["type"] | "new_skill"; value?: string };
 
 const Skills = ({
   collapsed,
-  toggleCollapse
+  toggleCollapse,
 }: {
   collapsed: boolean;
   toggleCollapse: Function;
@@ -27,28 +27,28 @@ const Skills = ({
   const { setValue, getValues } = useFormContext();
 
   const { fields: technical, append: technicalAppend } = useFieldArray({
-    name: 'technical_skills'
+    name: "technical_skills",
   });
   const { fields: tools, append: toolsAppend } = useFieldArray({
-    name: 'dev_tools'
+    name: "dev_tools",
   });
   const { fields: core, append: coreAppend } = useFieldArray({
-    name: 'core_subjects'
+    name: "core_subjects",
   });
   const { fields: languages, append: languagesAppend } = useFieldArray({
-    name: 'languages'
+    name: "languages",
   });
 
   const [newSkills, setNewSkills] = useState<string[]>([]);
 
   const isAlreadyAdded = (curr: any[], newVal: OnChangeValueType) => {
-    const item = curr?.find(skill => skill.value === newVal.name);
+    const item = curr?.find((skill) => skill.value === newVal.name);
     return item !== undefined;
   };
 
   const onChange = (val: OnChangeValueType) => {
     if (
-      ['technical_skills', 'dev_tools', 'core_subjects', 'languages'].includes(
+      ["technical_skills", "dev_tools", "core_subjects", "languages"].includes(
         val.type
       )
     ) {
@@ -58,24 +58,24 @@ const Skills = ({
     }
 
     switch (val.type) {
-      case 'technical_skills': {
+      case "technical_skills": {
         technicalAppend({ name: val.name });
         break;
       }
-      case 'dev_tools': {
+      case "dev_tools": {
         toolsAppend({ name: val.name });
         break;
       }
-      case 'core_subjects': {
+      case "core_subjects": {
         coreAppend({ name: val.name });
         break;
       }
-      case 'languages': {
+      case "languages": {
         languagesAppend({ name: val.name });
         break;
       }
       default: {
-        setNewSkills(curr => [...curr, val.name || (val.value as string)]);
+        setNewSkills((curr) => [...curr, val.name || (val.value as string)]);
       }
     }
   };
@@ -84,24 +84,24 @@ const Skills = ({
     array.filter((ele: any) => (ele?.value || ele) !== val);
 
   const handleDelete = (
-    skillType: Skill['type'] | 'new_skill',
+    skillType: Skill["type"] | "new_skill",
     val: string
   ) => {
-    if (skillType === 'new_skill') {
-      setNewSkills(curr => handleRemoveFromArray(curr, val));
+    if (skillType === "new_skill") {
+      setNewSkills((curr) => handleRemoveFromArray(curr, val));
     } else {
       setValue(skillType, handleRemoveFromArray(getValues(skillType), val));
     }
   };
 
-  const onDropSkill = (item: DragSkillItem, droppedOn: Skill['type']) => {
+  const onDropSkill = (item: DragSkillItem, droppedOn: Skill["type"]) => {
     onChange({ name: item.name, type: droppedOn });
     handleDelete(item.type, item.name);
     item.type = droppedOn;
   };
 
   const moveCard = useCallback(
-    (dragIndex: number, hoverIndex: number, skillType: Skill['type']) => {
+    (dragIndex: number, hoverIndex: number, skillType: Skill["type"]) => {
       const updatedList = getValues(skillType) as any[];
       const [movedItem] = updatedList.splice(dragIndex, 1);
       updatedList.splice(hoverIndex, 0, movedItem);
@@ -115,11 +115,11 @@ const Skills = ({
   return (
     <Grid
       sx={{
-        zIndex: 10
+        zIndex: 10,
       }}
     >
       <Heading
-        title='Skills'
+        title="Skills"
         icon={<SchoolIcon />}
         collapsed={collapsed}
         toggleCollapse={() => toggleCollapse()}
@@ -128,10 +128,10 @@ const Skills = ({
       {!collapsed && (
         <Grid
           sx={{
-            borderRadius: '0px 0px 20px 20px',
-            background: 'rgba(255, 255, 255, 0.10)',
-            backdropFilter: 'blur(20px)',
-            padding: '10px 15px 25px 15px'
+            borderRadius: "0px 0px 20px 20px",
+            background: "rgba(255, 255, 255, 0.10)",
+            backdropFilter: "blur(20px)",
+            padding: "10px 15px 25px 15px",
           }}
         >
           <SkillsContainer />
--- src/components/profile-details/skill-section.tsx
+++ src/components/profile-details/skill-section.tsx
@@ -1,20 +1,20 @@
-import { Grid, Typography } from '@mui/material';
-import React, { useCallback } from 'react';
-import DraggableChip, { DragSkillItem } from './dragable-chip';
-import { useDrop } from 'react-dnd';
-import { Skill } from '@/types';
-import { useFieldArray, useFormContext } from 'react-hook-form';
-import { firstLetterCapital } from '@/utils';
-import { righteous } from '@/font-family';
+import { Grid, Typography } from "@mui/material";
+import React, { Ref, useCallback } from "react";
+import DraggableChip, { DragSkillItem } from "./dragable-chip";
+import { useDrop } from "react-dnd";
+import { Skill } from "@/types";
+import { useFieldArray, useFormContext } from "react-hook-form";
+import { firstLetterCapital } from "@/utils";
+import { righteous } from "@/font-family";
 
 const SkillSection = ({
   onDropSkill,
   skillType,
   handleDelete,
-  list
+  list,
 }: {
   onDropSkill: Function;
-  skillType: Skill['type'];
+  skillType: Skill["type"];
   handleDelete: Function;
   list: any[];
 }) => {
@@ -22,20 +22,20 @@ const SkillSection = ({
 
   const [{ technicalIsOver }, technicalDropRef] = useDrop(() => ({
     accept: [
-      'core_subjects',
-      'languages',
-      'dev_tools',
-      'new_skill',
-      'technical_skills'
-    ].filter(skill => skill !== skillType),
+      "core_subjects",
+      "languages",
+      "dev_tools",
+      "new_skill",
+      "technical_skills",
+    ].filter((skill) => skill !== skillType),
     drop: (item: DragSkillItem) => onDropSkill(item, skillType),
-    collect: monitor => ({
-      technicalIsOver: !!monitor.isOver()
-    })
+    collect: (monitor) => ({
+      technicalIsOver: !!monitor.isOver(),
+    }),
   }));
 
   const moveCard = useCallback(
-    (dragIndex: number, hoverIndex: number, skillType: Skill['type']) => {
+    (dragIndex: number, hoverIndex: number, skillType: Skill["type"]) => {
       const updatedList = getValues(skillType) as any[];
       const [movedItem] = updatedList.splice(dragIndex, 1);
       updatedList.splice(hoverIndex, 0, movedItem);
@@ -46,39 +46,41 @@ const SkillSection = ({
     []
   );
 
-  return (
-    <Grid
-      ref={technicalDropRef}
-      sx={{
-        border: '1px solid',
-        borderColor: technicalIsOver ? '#ffffff80' : 'transparent',
-        background: technicalIsOver ? '#ffffff17' : 'transparent',
-        borderRadius: '10px',
-        padding: '5px'
-      }}
-    >
-      <Typography mb='5px' fontFamily={righteous.style.fontFamily}>
-        {firstLetterCapital(skillType.split('_').join(' '))}
-      </Typography>
+  return technicalDropRef(
+    <div>
       <Grid
+        // ref={technicalDropRef}
         sx={{
-          display: 'flex',
-          gap: '10px',
-          flexWrap: 'wrap'
+          border: "1px solid",
+          borderColor: technicalIsOver ? "#ffffff80" : "transparent",
+          background: technicalIsOver ? "#ffffff17" : "transparent",
+          borderRadius: "10px",
+          padding: "5px",
         }}
       >
-        {list.map((skill: any, ind) => (
-          <DraggableChip
-            key={skill.id}
-            onDelete={() => handleDelete(skillType, skill.name)}
-            label={skill.name}
-            skillType={skillType}
-            index={ind}
-            moveCard={moveCard}
-          />
-        ))}
+        <Typography mb="5px" fontFamily={righteous.style.fontFamily}>
+          {firstLetterCapital(skillType.split("_").join(" "))}
+        </Typography>
+        <Grid
+          sx={{
+            display: "flex",
+            gap: "10px",
+            flexWrap: "wrap",
+          }}
+        >
+          {list.map((skill: any, ind) => (
+            <DraggableChip
+              key={skill.id}
+              onDelete={() => handleDelete(skillType, skill.name)}
+              label={skill.name}
+              skillType={skillType}
+              index={ind}
+              moveCard={moveCard}
+            />
+          ))}
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 };
 
