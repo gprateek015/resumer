ad166cdf91566873e88dea1b6fb1d7593ca71852
Prateek Goyal <gprateek015@gmail.com>
2024-08-13 00:33:49 +0530

    feat. testing cookie in prod

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,25 +1,26 @@
-import axios from 'axios';
+import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 Axios.interceptors.request.use(
-  config => {
+  (config) => {
+    // This is important for sending cookies
     config.withCredentials = true;
     return config;
   },
-  error => {
+  (error) => {
     return Promise.reject(error);
   }
 );
 
 Axios.interceptors.response.use(
-  response => {
+  (response) => {
     return response;
   },
-  error => {
-    if (error.name === 'AxiosError')
+  (error) => {
+    if (error.name === "AxiosError")
       return Promise.reject({ ...error, message: error.response.data.error });
     return Promise.reject(error);
   }
